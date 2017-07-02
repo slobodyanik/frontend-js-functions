3e5b4b12263d2fb542d6d5dd35221306676f5965
slobodyanik <slobodyanik.vyacheslav@gmail.com>
2017-07-02 11:00:16 +0300

    done sum. issues with max and min functions.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-﻿
+﻿function f1() {
+	var sum = 0;
+	for (var i = 0; i < arguments.length; i++) {
+		if (typeof arguments[i] !== 'number')
+			continue
+		sum = sum + arguments[i]
+
+	}
+	console.log(sum);
+}
+f1(48, 351, 56, 30, 85, 96);
+
+function max(Array) {
+	
+	if (Array === undefined) {
+		return;
+	} else {
+
+
+		var MaxNum = Array[0];
+		for ( var i = 0; i < Array.length; i++) {
+			if (Array[i] >= MaxNum) {
+				MaxNum = Array[i];
+			}
+
+		}
+		return MaxNum;
+	}
+}
+max([98, 93, 75, 73, 6666]);
+
+
+
+// function min(array) {
+// var MinNum = Array[0];
+// for(var i = 0; i < Array.length; i++);
+// if (Array[i] < MinNum){
+// 	MinNum = Array[i]
+// }
+// }
+
+
+
